b46ba8326b7a00ad810a3f70adeb51b1ec2a4614
mariano techera <marianoeduardotechera@gmail.com>
2023-08-19 20:33:04 +0200

    V 0.3

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import { CartContext } from '../context/ShoppingCartContext'
 
 const Cart = () => {
-  const { cart, setCart } = useContext(CartContext)
-
+  
   const removeList = () => {//implementa la funcionalidad para dejar el carrito vacío
     cart.clear()
     localStorage.setItem("Cart", JSON.stringify(cart))
-}
-const deleteItem = () => { //implementa la funcionalidad para borrar un producto del carrito
-    let item = cart.findIndex((prod) => prod.id === (e.target.id))
-    if (item > -1) {
-        cart.splice(item, 1)
-        localStorage.setItem("Cart", JSON.stringify(cart))
+  }
+  const deleteItem = (id) => { //implementa la funcionalidad para borrar un producto del carrito
+    let indice = cart.findIndex((prod) => prod.id === (e.target.id))
+    if (indice > -1) {
+      cart.splice(indice, 1)
+      localStorage.setItem("Cart", JSON.stringify(cart))
     }
-}
+  }
 
   return (
     <div>
