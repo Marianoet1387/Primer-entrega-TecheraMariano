96efc0627cd7f31a4d8bd114bb8d5cc7341c2fb7
mariano techera <marianoeduardotechera@gmail.com>
2023-08-23 10:48:27 +0200

    V 0.4

--- src/context/ShoppingCartContext.jsx
+++ src/context/ShoppingCartContext.jsx
@@ -5,7 +5,7 @@ const ShoppingCartProvider = ({ children }) => {
   const [cart, setCart] = useState([])
   
   return (
-    <CartContext.Provider value={[cart, setCart, count, setCount]}>
+    <CartContext.Provider value={[cart, setCart]}>
       {children}
     </CartContext.Provider>
   )
